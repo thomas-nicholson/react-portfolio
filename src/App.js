5d3c164d361d483470d9c1d76c66639a3e929a88
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-07-16 14:52:02 +1000

    Abstracted projects array to separate utils file

--- src/App.js
+++ src/App.js
@@ -2,47 +2,11 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Project from './components/Project';
 
-
+import projects from './utils/projects';
 
 function App() {
 
-  const projects = [
-    {
-      title:"Gym Club",
-      img: "",
-      gitLink: "",
-      deployedLink: "",
-      description: ""
-    },
-    {
-      title: "Reacharity",
-      img: "./assets/reacharity-screenshot.png",
-      gitLink: "",
-      deployedLink: "",
-      description: ""
-    },
-    {
-      title:"Weather Dashboard",
-      img: "./assets/weather-dashboard-screenshot.png",
-      gitLink: "",
-      deployedLink: "",
-      description: ""
-    },
-    {
-      title:"Code Quiz",
-      img: "./assets/code-quiz-screenshot.png",
-      gitLink: "",
-      deployedLink: "",
-      description: ""
-    },
-    {
-      title:"Gym Club",
-      img: "./assets/gym-club-screenshot.png",
-      gitLink: "",
-      deployedLink: "",
-      description: ""
-    },
-  ];
+  
 
   return (
     <div>
---
+++ src/utils/projects.js
@@ -0,0 +1,39 @@
+const projects = [
+    {
+      title:"Gym Club",
+      img: "",
+      gitLink: "",
+      deployedLink: "",
+      description: ""
+    },
+    {
+      title: "Reacharity",
+      img: "./assets/reacharity-screenshot.png",
+      gitLink: "",
+      deployedLink: "",
+      description: ""
+    },
+    {
+      title:"Weather Dashboard",
+      img: "./assets/weather-dashboard-screenshot.png",
+      gitLink: "",
+      deployedLink: "",
+      description: ""
+    },
+    {
+      title:"Code Quiz",
+      img: "./assets/code-quiz-screenshot.png",
+      gitLink: "",
+      deployedLink: "",
+      description: ""
+    },
+    {
+      title:"Gym Club",
+      img: "./assets/gym-club-screenshot.png",
+      gitLink: "",
+      deployedLink: "",
+      description: ""
+    },
+  ];
+
+  export default projects;
