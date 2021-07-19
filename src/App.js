5f96a6e35f322b940db5ed5a91fdfbe3cad95e93
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-07-19 23:02:22 +1000

    Added styling to center all text

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Body from './components/Body';
 
+const style = {
+  textAlign: "center"
+}
+
 function App() {
   return (
-    <div>
+    <div style={style}>
       <Header />
       <Body />
       <Footer />
