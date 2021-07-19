1195aa01943c0483d389436175430dae6b79cb50
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-07-20 08:18:08 +1000

    Added basic skeleton for navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,26 @@
+const style = {
+    list: {
+        listStyleType: "none",
+        margin: 0,
+        padding: 0,
+    },
+    item: {
+        display: "inline",
+        padding: 10,
+    }
+}
+
 function Navigation() {
     return(
-        <h2>Navigation</h2>
+        <div>
+            <h2>Navigation</h2>
+            <ul style={style.list}>
+                <li style={style.item}><a href="#home">Home</a></li>
+                <li style={style.item}><a href="#aboutme">About Me</a></li>
+                <li style={style.item}><a href="#portfolio">Portfolio</a></li>
+                <li style={style.item}><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
     );
 }
 
