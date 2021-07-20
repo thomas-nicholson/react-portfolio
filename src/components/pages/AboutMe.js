53744a6073c49623ec6c11fa2c5f9d8f4d3a7b53
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-07-20 20:08:36 +1000

    Added styling and paragraph elements

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,7 +1,15 @@
+const style = {
+    textAlign: "justify",
+    textJustify: "inter-word"
+}
+
 function AboutMe() {
     return(
-        <div>
+        <div style={style} >
             <h1>About Me</h1>
+            <p>Welcome to my portfolio page, my name is Thomas Nicholson.</p>
+            <p>I'm a 25 year old web developer. My interests include health, nutrition, education, business and politics.</p>
+            <p>Check out my portfolio or contact me with the links at the bottom if you'd like.</p>
         </div>
     )
 }
