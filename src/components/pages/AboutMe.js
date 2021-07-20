9a6c8056f028f07f4329400b6fffb745609b3948
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-07-20 23:26:15 +1000

    Added developer image. changed styling to be conducive to developer image. Added more information to About Me

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,17 +1,27 @@
 const style = {
-    textAlign: "justify",
-    textJustify: "inter-word",
-    width: "80%",
-    flexGrow: 4
+    container: {
+        width: "80%",
+        flexGrow: 4
+    },
+    img: {
+        width: "200px",
+        borderRadius: "15px"
+    },
+    text: {
+        fontSize: "20px"
+    }
+    
 }
 
 function AboutMe() {
     return(
-        <div style={style} >
+        <div style={style.container} >
+            <img style={style.img} src="./assets/me.jpg" />
             <h1>About Me</h1>
-            <p>Welcome to my portfolio page, my name is Thomas Nicholson.</p>
-            <p>I'm a 25 year old web developer. My interests include health, nutrition, education, business and politics.</p>
-            <p>Check out my portfolio or contact me with the links at the bottom if you'd like.</p>
+            <p style={style.text}>Welcome to my portfolio page, my name is Thomas Nicholson.</p>
+            <p style={style.text}>I'm a 25 year old web developer. My interests include health, nutrition, education, business and politics.</p>
+            <p style={style.text}>Currently I'm finalising my web development course and working on two big exciting projects</p>
+            <p style={style.text}>Check out my portfolio, contact me, or peruse the links at the bottom.</p>
         </div>
     )
 }
