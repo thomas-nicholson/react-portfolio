019da2646f50bd53cfb595f14f100449a363f5a2
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-07-20 21:44:34 +1000

    Added content and styling for Contact page component

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,7 +1,32 @@
+const style = {
+    container: {
+        textAlign: "center",
+    },
+    list: {
+        listStyleType: "none",
+        margin: 0,
+        padding: 0,
+        width: "100%"
+    },
+    item: {
+        display: "inline",
+        padding: 10,
+        color: "black"
+    }
+}
+
 function Contact() {
     return(
-        <div>
-            <h1>Contact</h1>
+        <div style={style.container} >
+            <h2>Contact</h2>
+            <ul style={style.list}>
+                <li style={style.item}><a href="#">Phone</a></li>
+                <li style={style.item}><a href="#">Email</a></li>
+                <li style={style.item}><a href="#">Resume</a></li>
+                <li style={style.item}><a href="#">LinkedIn</a></li>
+                <li style={style.item}><a href="#">Github</a></li>
+                <li style={style.item}><a href="#">Twitter</a></li>
+            </ul>
         </div>
     )
 }
