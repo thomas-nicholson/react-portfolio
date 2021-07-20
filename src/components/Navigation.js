fa55be3d737905d8b9058b3e508d4fe2cb4e74a7
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-07-20 21:47:30 +1000

    added horizontal line to navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -13,13 +13,13 @@ const style = {
 function Navigation({ currentPage, onPageChange }) {
     return(
         <div>
-            <h2>Navigation</h2>
             <ul style={style.list}>
                 <li style={style.item}><a href="#aboutme" onClick={() => onPageChange('AboutMe')}>About Me</a></li>
                 <li style={style.item}><a href="#now" onClick={() => onPageChange('Now')}>Now</a></li>
                 <li style={style.item}><a href="#portfolio" onClick={() => onPageChange('Portfolio')}>Portfolio</a></li>
                 <li style={style.item}><a href="#contact" onClick={() => onPageChange('Contact')}>Contact</a></li>
             </ul>
+            <hr />
         </div>
     );
 }
